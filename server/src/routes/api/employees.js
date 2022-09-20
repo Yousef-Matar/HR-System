98e4207c6872cbb078145208d88ccfa48c4ad265
Yousef Matar <yousefmatar@outlook.com>
2022-09-20 13:08:00 +0200

    refinements

--- server/src/routes/api/employees.js
+++ server/src/routes/api/employees.js
@@ -4,19 +4,12 @@ const employee = require('../../models/employee')
 const router = express.Router()
 
 // Get Employees
-router.get('/', async (req, res) => {
-	employee
-		.find()
-		.then((result) => res.send(result))
-		.catch((err) => console.error(err))
-})
-// Get One Employee
-router.get('/:id', async (req, res) => {
-	employee
-		.findById(req.params.id)
-		.then((result) => res.send(result))
-		.catch((err) => console.error(err))
-})
+//router.get('/', async (req, res) => {
+//	employee
+//		.find()
+//		.then((result) => res.send(result))
+//		.catch((err) => console.error(err))
+//})
 // Add Employee
 router.post('/', async (req, res) => {
 	const Employee = new employee(req.body)
@@ -27,20 +20,20 @@ router.post('/', async (req, res) => {
 })
 
 // Edit Employee
-router.patch('/:id', async (req, res) => {
-	employee
-		.findOneAndUpdate(req.params.id, {
-			username: req.body.username,
-			password: req.body.password,
-			firstName: req.body.firstName,
-			lastName: req.body.lastName,
-			role: req.body.role,
-			status: req.body.status,
-			yearlyVacation: req.body.yearlyVacation,
-			attendance: req.body.attendance,
-		},{new: true})
-		.then((result) => res.send(result))
-		.catch((err) => console.error(err))
-})
+//router.patch('/:id', async (req, res) => {
+//	employee
+//		.findOneAndUpdate(req.params.id, {
+//			username: req.body.username,
+//			password: req.body.password,
+//			firstName: req.body.firstName,
+//			lastName: req.body.lastName,
+//			role: req.body.role,
+//			status: req.body.status,
+//			yearlyVacation: req.body.yearlyVacation,
+//			attendance: req.body.attendance,
+//		},{new: true})
+//		.then((result) => res.send(result))
+//		.catch((err) => console.error(err))
+//})
 
 module.exports = router
