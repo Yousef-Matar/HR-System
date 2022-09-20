7cda05b8e52908a220284268c8d9f7a025ff2b8b
Yousef Matar <yousefmatar@outlook.com>
2022-09-20 20:35:50 +0200

    added remaining fields in employee model

--- server/src/models/employeeModel.js
+++ server/src/models/employeeModel.js
@@ -13,6 +13,22 @@ const employeeSchema = new Schema(
 			required: true,
 			bcrypt: true,
 		},
+		firstName: {
+			type: String,
+			required: true,
+		},
+		lastName: {
+			type: String,
+			required: true,
+		},
+		role: {
+			type: String,
+			required: true,
+		},
+		status: {
+			type: String,
+			required: true,
+		},
 		yearlyVacation: {
 			type: Number,
 			required: true,
